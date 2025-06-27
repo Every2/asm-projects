139f4b2ee64e0fe54792574fe1f5628d71c97252
Every2 <christiansantoslima21@gmail.com>
2025-06-26 22:06:01 -0300

    add buffer and to integer function

--- calculator/src/calc.asm
+++ calculator/src/calc.asm
@@ -4,29 +4,49 @@ SYS_WRITE equ 4
 section .data
 msg: db "Please enter a number: "
 msg_len: equ $-msg
+op_msg: db "Choose one operation: +, -, *, /", 10
+op_len: equ $-op_msg
 
 section .bss
 buffer: resb 4
 buffer_len: resd 1
+num: resb 4
 
 section .text
 global _start
 _start:
-  push ebp
-  mov ebp, esp
   push msg_len
   push msg
   call write_msg
   add esp, 8
+
   call read_input
-  push buffer_len
-  push buffer
+
+  call to_integer
+  mov num, eax
+  xor eax, eax
+  call clear_buffer
+
+  push op_len
+  push op_msg
   call write_msg
-  add esp, 8
+ 
   mov eax, 1
   xor ebx, ebx
   int 0x80
-  pop ebp
+
+clear_buffer:
+  mov ecx, 1
+  lea edx, [buffer]
+  xor eax, eax
+  .loop:
+    mov [edx + ecx * 4], eax
+    dec ecx
+    jnz .loop
+  ret
+
+jump_if_equal:
+  
 
 write_msg:
   push ebp
@@ -48,3 +68,26 @@ read_input:
   int 0x80
   ret
   
+to_integer:
+  xor eax, eax
+  xor ecx, ecx
+ 
+  .loop:
+    mov bl, [buffer + ecx]
+    cmp bl, 10
+    je .done
+    cmp bl, 0
+    je .done
+    
+    sub bl, '0'
+    cmp bl, 9
+    ja .done
+    
+    imul eax, eax, 10
+    add eax, ebx
+
+    inc ecx
+    jmp .loop
+   
+   .done:
+     ret
